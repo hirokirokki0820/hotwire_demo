8ae7624560f3f2d154bcbf8922c5997e2068a953
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-23 15:32:32 +0900

    Add: Bootstrap でデザインを整える

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   resources :rabbits
   resources :dogs
   resources :cats
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root to: redirect("/cats")
 end
