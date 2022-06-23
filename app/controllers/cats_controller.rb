2fb7c3ff2c03203a0925344688855c101f7246e8
hirokirokki0820 <hirokirokki0820@gmail.com>
2022-06-23 22:02:24 +0900

    Update: 編集の Turbo Streams 化

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -44,7 +44,8 @@ class CatsController < ApplicationController
   # PATCH/PUT /cats/1
   def update
     if @cat.update(cat_params)
-      redirect_to @cat, notice: "ネコを更新しました。"
+      # リダイレクトを削除（リダイレクトがないと暗黙的に`render`が実行される）
+      # redirect_to @cat, notice: "ねこを更新しました。"
     else
       render :edit, status: :unprocessable_entity
     end
